8557607ba24e58b3236502a2ef54a00af7078a1d
Jakub Walczak <jakub_walczak@tlen.pl>
2020-11-12 23:01:23 +0100

    setTimer/setInterval

--- ex-node.js
+++ ex-node.js
@@ -1,3 +1,10 @@
 setTimeout( () => {
     console.log("excersise in node")
 }, 3000)
+
+const interval = setInterval( () => {
+    console.log("interval");
+    setTimeout( () => {
+        clearInterval(interval);
+    }, 10*1000)
+}, 1000)
